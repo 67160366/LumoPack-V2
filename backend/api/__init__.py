092da258e2e7f2acd40d8746716421d3c4fb95e7
67160366 <67160366@go.buu.ac.th>
2026-02-11 12:12:56 +0700

    fix

--- backend/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API Package
-FastAPI endpoints for LumoPack Chatbot
-"""
-
-from .chat import router as chat_router
-from .pricing import router as pricing_router
-
-__all__ = ["chat_router", "pricing_router"]
